7587eda224fa22186229552bde045112bb3646bd
Killian Morere <killian.morere@fms-ea.com>
2025-10-29 11:25:51 +0100

    Added student display in the console and fixed the program not correctly asking for the grades.

--- src/array/BaseEx4Array.java
+++ src/array/BaseEx4Array.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.TreeMap;
+import java.util.Set;
 
 public class BaseEx4Array {
 
 	//static HashMap<String, int[]> students = new HashMap<>();
+	// A unique list containing each student's name and grades.
 	static TreeMap<String, int[]> students = new TreeMap<>();
 	
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		
 		while(students.size() < 3) {
+			DisplayStudents();
 			SetName(reader);
 			System.out.println();
 		}
@@ -37,8 +40,27 @@ public class BaseEx4Array {
 		}
 	}
 	
-	private static void SortStudents() {
+	/// Display each student's name in the console in order.
+	private static void DisplayStudents() {
+		if (students.size() == 0)
+			return;
 		
+		for (String student : students.keySet()) {
+			System.out.print("[");
+			System.out.print(student);
+			System.out.print("] ");
+		}
+		System.out.println();
+	}
+	
+	/// Reverse display of the students list.
+	private static void Reverse() {
+		for (String student : students.descendingKeySet()) {
+			System.out.print("[");
+			System.out.print(student);
+			System.out.print("] ");
+		}
+		System.out.println();
 	}
 	
 	/// Gives the average grade from a number array.
@@ -57,12 +79,12 @@ public class BaseEx4Array {
 	private static boolean GetStudent(String _name) {
 		if(students.containsKey(_name)) {
 			if(students.get(_name).length > 0)
-				System.out.println(_name + " " + 
-						GetMedian(students.get(_name)) + " " + 
-						students.get(_name));
-			else
-				return false;
+				System.out.println(_name + " | " + 
+						GetMedian(students.get(_name)) + " | " + 
+						Arrays.toString(students.get(_name)));
 		}
+		else
+			return false;
 		return true;
 	}
 	
@@ -70,7 +92,7 @@ public class BaseEx4Array {
 	private static String SetName(Scanner _scanner) {
 		System.out.println("Insert the student's name and surname in a single line :");
 		String name = _scanner.nextLine();
-		
+
 		if(!GetStudent(name)) {
 			int[] grades = SetGrade(_scanner, name);
 			
